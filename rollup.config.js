fae89e90f21c48d400a6bcb8d831ed30f02461b1
Ben Werner <ben.werner01@gmail.com>
2021-01-17 12:43:18 +0000

    rollup postcss

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
 
 const packageJson = require('./package.json');
 
@@ -24,5 +25,6 @@ export default {
     resolve(),
     commonjs(),
     typescript({ }),
+    postcss({ plugins: [] }),
   ],
 };
