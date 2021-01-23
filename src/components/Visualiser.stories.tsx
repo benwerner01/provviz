fb13f14759ce622695a647f690bba87757786d13
Ben Werner <ben.werner01@gmail.com>
2021-01-23 12:54:18 +0000

    fix(accessibility): visualisation font color accessibility

--- src/components/Visualiser.stories.tsx
+++ src/components/Visualiser.stories.tsx
@@ -1,15 +1,53 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Story, Meta } from '@storybook/react';
 import Visualiser, { VisualiserProps } from './Visualiser';
-import { document1, document2 } from '../lib/examples';
+import {
+  document1, document2, document3, document4,
+} from '../lib/examples';
 
-export const Example1: Story<VisualiserProps> = (args) => (
-  <Visualiser {...args} document={document1} />
+const Uncontrolled: React.FC<VisualiserProps & { initialDocument: object }> = ({
+  initialDocument, ...args
+}) => {
+  const [document, setDocument] = useState<object>(initialDocument);
+
+  useEffect(() => {
+    setTimeout(() => setDocument((prev) => ({
+      ...prev,
+      prefix: { ...(prev as any).prefix, best: 'value' },
+    })), 5000);
+  }, []);
+
+  return (
+    <Visualiser
+      {...args}
+      document={document}
+      onChange={(change) => {
+        console.log('Uncontrolled Visualiser got updated: ', change);
+        setDocument(change);
+      }}
+    />
+  );
+};
+
+export const Example1Controlled: Story<VisualiserProps> = (args) => (
+  <Visualiser {...args} document={document1} onChange={null} />
+);
+
+export const Example1Uncontrolled: Story<VisualiserProps> = (args) => (
+  <Uncontrolled initialDocument={document1} {...args} />
 );
 
 export const Example2: Story<VisualiserProps> = (args) => (
-  <Visualiser {...args} document={document2} />
+  <Visualiser {...args} document={document2} onChange={null} />
+);
+
+export const Example3: Story<VisualiserProps> = (args) => (
+  <Visualiser {...args} document={document3} onChange={null} />
+);
+
+export const Example4: Story<VisualiserProps> = (args) => (
+  <Visualiser {...args} document={document4} onChange={null} />
 );
 
 const meta: Meta<VisualiserProps> = {
@@ -18,7 +56,7 @@ const meta: Meta<VisualiserProps> = {
   argTypes: {
     height: {
       name: 'height',
-      defaultValue: 1000,
+      defaultValue: 900,
       control: {
         type: 'range', min: 50, max: 1500, step: 50,
       },
